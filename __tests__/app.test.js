73af2e52056de9eac696b1442f1609cc24cc1706
Diyana Mendoza <>
2022-02-07 16:03:55 -0800

    Add users post route w passing test for creating a new user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,12 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+// mock user for testing
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345',
+};
+
 describe('backend routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +16,15 @@ describe('backend routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
 });
